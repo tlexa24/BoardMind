6e097601c06ab2c305951d517f842351621d434d
tlexa24 <tlexa2497@gmail.com>
2025-08-23 19:50:45 -0400

    Setup React to connect with Flask backend, with CORS

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return "Hello, BoardMind!"
+
+@app.route('/api/message')
+def message():
+    return jsonify(message="Hello from Flask!")
 
 
 if __name__ == '__main__':
